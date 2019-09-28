1548c808ddea77f917883171856c6ea96180eaf9
tjmtmmnk <tsujimoto027@gmail.com>
2019-09-29 00:42:11 +0900

    142

---
+++ atcoder/ABC/142/B.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+static const ll maxLL = (ll) 1 << 62;
+const ll MOD = 1000000007, INF = 1e18;
+
+template<typename T1, typename T2>
+bool pairCompare(const pair<T1, T2> &firstElof, const pair<T1, T2> &secondElof) {
+    return firstElof.second > secondElof.second;
+}
+
+int mapMaxValue(std::map<ll, int> m) {
+    int max = 0;
+    for (std::map<ll, int>::iterator it = m.begin(); it != m.end(); it++) {
+        if (max < it->second) max = it->second;
+    }
+    return max;
+}
+
+int main(void) {
+    int n, k;
+    cin >> n >> k;
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int tmp;
+        cin >> tmp;
+        if(tmp >= k) ans++;
+    }
+    cout << ans << endl;
+}
--- atcoder/ABC/142/D.cpp
+++ atcoder/ABC/142/D.cpp
@@ -44,5 +44,9 @@ template<typename TP=int, typename TPOW=int>
 vector<pair<TP, TPOW>> factorize(TP n) {vector<pair<TP, TPOW>> res;for (TP i = 2; i * i < n; ++i) {if (n % i) continue;res.emplace_back(i, 0);while (n % i == 0) {n /= i;res.back().second++;}}if (n != 1) res.emplace_back(n, 1);return res;}
 
 int main(void) {
-
+    ll a, b;
+    cin >> a >> b;
+    ll g = gcd<ll>(a, b);
+    auto f = factorize<ll, int>(g);
+    cout << f.size() + 1 << endl;
 }
